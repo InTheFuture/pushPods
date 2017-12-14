69cabfb27598db2ba1e9711fd15e2a67036bb0f3
InTheFuture <990437240@qq.com>
2017-12-14 14:56:01 +0800

    init

---
+++ LocationTool/LocationTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationTool
+//
+//  Created by 刘欢 on 2017/12/14.
+//  Copyright © 2017年 刘欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationTool/LocationTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationTool
+//
+//  Created by 刘欢 on 2017/12/14.
+//  Copyright © 2017年 刘欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
